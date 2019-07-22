5a5f5f60426687e73d0093020f6d10c0a2bc6369
xiangkejun <kjunxiang@126.com>
2019-07-22 13:54:23 +0800

    Update BFS.py

--- 常用算法/BFS.py
+++ 常用算法/BFS.py
@@ -10,17 +10,17 @@ graph = {
 # print(graph["A"])
 def BFS(graph,s):
     queue = []
-    queue.append(s)
+    queue.append(s) # "A"
     seen = set() # 集合
-    seen.add(s)
+    seen.add(s) # A BC 
     while (len(queue)>0 ) :
-        vertex = queue.pop(0) # 头顶元素
-        nodes = graph[vertex]
+        vertex = queue.pop(0) # 头顶元素 "A"
+        nodes = graph[vertex] # "B""C" |
         for w in nodes:
             if w not in seen:
-                queue.append(w)
-                seen.add(w)
-        print(vertex)
+                queue.append(w) # B C |D
+                seen.add(w) # B C |D
+        print(vertex) # A
 
 print(BFS(graph,"A")) # A B C D E F
 print(BFS(graph,"E")) # E C D A B F
